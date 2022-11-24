7f4a6269891a3847349b34a7da80f321e3058909
Paul Lessing <lessing.paul+github@gmail.com>
2022-11-24 22:36:23 +0700

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { WORDLISTS } from '../wordlists/english';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<ol>
+  {#each WORDLISTS['English (Duet)'] as word}
+    <li>{word}</li>
+  {/each}
+</ol>
