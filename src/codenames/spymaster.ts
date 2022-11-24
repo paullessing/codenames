4157443a5e3b56bc18d1eb438b4f2b1196dfb32d
Paul Lessing <lessing.paul+github@gmail.com>
2022-11-24 23:27:21 +0700

    Add logic to generate Duet spymaster views

---
+++ src/codenames/spymaster.ts
@@ -0,0 +1,64 @@
+import seedrandom from 'seedrandom';
+
+export enum DuetFieldType {
+  Bystander = 'Bystander',
+  Agent = 'Agent',
+  Assassin = 'Assassin',
+}
+
+export type DuetField = readonly [agentA: DuetFieldType, agentB: DuetFieldType];
+
+const DUET_ALL_FIELDS = [
+  [DuetFieldType.Agent, DuetFieldType.Agent],
+  [DuetFieldType.Agent, DuetFieldType.Agent],
+  [DuetFieldType.Agent, DuetFieldType.Agent],
+
+  [DuetFieldType.Agent, DuetFieldType.Bystander],
+  [DuetFieldType.Agent, DuetFieldType.Bystander],
+  [DuetFieldType.Agent, DuetFieldType.Bystander],
+  [DuetFieldType.Agent, DuetFieldType.Bystander],
+  [DuetFieldType.Agent, DuetFieldType.Bystander],
+
+  [DuetFieldType.Bystander, DuetFieldType.Agent],
+  [DuetFieldType.Bystander, DuetFieldType.Agent],
+  [DuetFieldType.Bystander, DuetFieldType.Agent],
+  [DuetFieldType.Bystander, DuetFieldType.Agent],
+  [DuetFieldType.Bystander, DuetFieldType.Agent],
+
+  [DuetFieldType.Bystander, DuetFieldType.Bystander],
+  [DuetFieldType.Bystander, DuetFieldType.Bystander],
+  [DuetFieldType.Bystander, DuetFieldType.Bystander],
+  [DuetFieldType.Bystander, DuetFieldType.Bystander],
+  [DuetFieldType.Bystander, DuetFieldType.Bystander],
+  [DuetFieldType.Bystander, DuetFieldType.Bystander],
+  [DuetFieldType.Bystander, DuetFieldType.Bystander],
+
+  [DuetFieldType.Agent, DuetFieldType.Assassin],
+  [DuetFieldType.Bystander, DuetFieldType.Assassin],
+  [DuetFieldType.Assassin, DuetFieldType.Assassin],
+  [DuetFieldType.Assassin, DuetFieldType.Agent],
+  [DuetFieldType.Assassin, DuetFieldType.Bystander],
+] as const;
+
+function numberUpTo(rng: seedrandom.PRNG, exclusiveMaximum: number): number {
+  return Math.floor(rng.quick() * exclusiveMaximum);
+}
+
+export function generateDuetSpymaster(seed: string): readonly DuetField[] {
+  const rng = seedrandom(seed);
+
+  // Randomise the list using Fisher-Yates shuffle
+  const fields = DUET_ALL_FIELDS.slice();
+  for (let i = fields.length - 1; i >= 1; i--) {
+    const newPosition = numberUpTo(rng, i);
+    // console.log('swapping', i, newPosition);
+    if (i === newPosition) {
+      continue;
+    }
+    const tempValue = fields[newPosition];
+    fields[newPosition] = fields[i];
+    fields[i] = tempValue;
+  }
+
+  return fields;
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,67 @@
 <script lang="ts">
-  import { WORDLISTS } from '../wordlists/english';
+  // import { WORDLISTS } from '../wordlists/english';
+
+  const now = new Date();
+
+  let seed = `${now.getMonth() + 1}${now.getDate()}`;
+  import { generateDuetSpymaster } from '../codenames/spymaster';
+
+  $: spymaster = generateDuetSpymaster(seed);
+
+  const fields: number[][] = [];
+  for (let i = 0; i < 5; i++) {
+    let row: number[] = [];
+    for (let j = 0; j < 5; j++) {
+      row.push(i * 5 + j);
+    }
+    fields.push(row);
+  }
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Code Names</h1>
 
-<ol>
-  {#each WORDLISTS['English (Duet)'] as word}
-    <li>{word}</li>
+<!--<ol>-->
+<!--  {#each WORDLISTS['English (Duet)'] as word}-->
+<!--    <li>{word}</li>-->
+<!--  {/each}-->
+<!--</ol>-->
+
+<input bind:value={seed} />
+
+<table style="margin-bottom: 2rem;">
+  {#each fields as row}
+    <tr>
+      {#each row as entry}
+        <td class="cell {spymaster[entry][0]}">&nbsp;</td>
+      {/each}
+    </tr>
   {/each}
-</ol>
+</table>
+<table>
+  {#each fields as row}
+    <tr>
+      {#each row as entry}
+        <td class="cell {spymaster[entry][1]}">&nbsp;</td>
+      {/each}
+    </tr>
+  {/each}
+</table>
+
+<style lang="scss">
+  .cell {
+    width: 24px;
+    height: 16px;
+
+    &.Agent {
+      background-color: green;
+    }
+
+    &.Assassin {
+      background-color: #222;
+    }
+
+    &.Bystander {
+      background-color: beige;
+    }
+  }
+</style>
