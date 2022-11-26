daca71881941092cefd1adcd52cde23b038e8e55
Paul Lessing <lessing.paul+github@gmail.com>
2022-11-26 08:44:02 +0700

    Add page with slug in the URL

---
+++ src/routes/duet/[seed]/[player]/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+  import DuetSpymasterView from '../../DuetSpymasterView.svelte';
+
+  const { seed, player } = $page.params;
+</script>
+
+<DuetSpymasterView {seed} {player} />
